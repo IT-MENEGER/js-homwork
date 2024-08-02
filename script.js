9239c287a859e08f3cffba92365e50b77da71e3f
Jamshid <jamshidgafforov2@gmail.com>
2024-08-02 18:55:47 +0500

    to write reduce function

---
+++ script.js
@@ -0,0 +1,21 @@
+function myreduce(arr, cb, initialValue) {
+  let res = initialValue;
+  for (let i = 0; i < arr.length; i++) {
+    res = cb(res, arr[i], i, arr);
+  }
+
+  return res;
+}
+
+
+let num = [1, 34, 23, 45, 53];
+let resul = myreduce(
+  num,
+  (acc, currentvalue, i, arr) => {
+    acc.push([currentvalue, arr[i + 1]]);
+    return acc;
+  },
+  []
+  
+);
+console.log(resul);
--- task-1/task-1.js
+++
@@ -1,8 +0,0 @@
-const res = document.getElementById("res");
-const inp = document.getElementById("input");
-
-function getfoto() {
-  let inputvalue = inp.value;
-  res.innerHTML += `<img src="${inputvalue}" alt="rasm">`;
-  inputvalue = '';
-}
